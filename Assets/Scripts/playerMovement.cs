af37a30747e57a34b00bbb526f534e66ebdc4cd2
Miyaki Truong <32574@ma-web.nl>
2022-03-15 13:07:42 +0100

    Previous

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -6,63 +6,77 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
 
-    private Rigidbody rb;
+    float mainSpeed = 25.0f; //regular speed
+    float shiftAdd = 250.0f; //multiplied by how long shift is held.  Basically running
+    float maxShift = 50.0f; //Maximum speed when holding shift
+    private float totalRun = 1.0f;
+    public int Boundary = 50;
+    public int speed = 5;
 
-    // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
-        Debug.Log("Hello World! playerMovement script loaded.");
-
-        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
     }
-
-    // Update is called once per frame
     void Update()
     {
 
+        //Keyboard commands
+        float f = 0.0f;
+        Vector3 p = GetBaseInput();
+        if (p.sqrMagnitude > 0)
+        { // only move while a direction key is pressed
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                totalRun += Time.deltaTime;
+                p = p * totalRun * shiftAdd;
+                p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
+                p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
+                p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
+            }
+            else
+            {
+                totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
+                p = p * mainSpeed;
+            }
+
+            p = p * Time.deltaTime;
+            Vector3 newPosition = transform.position;
+            if (Input.GetKey(KeyCode.Space))
+            { //If player wants to move on X and Z axis only
+                transform.Translate(p);
+                newPosition.x = transform.position.x;
+                newPosition.z = transform.position.z;
+                transform.position = newPosition;
+            }
+            else
+            {
+                transform.Translate(p);
+            }
+        }
+
+    }
+
+    private Vector3 GetBaseInput()
+    { //returns the basic values, if it's 0 than it's not active.
+        Vector3 p_Velocity = new Vector3();
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(new Vector3(0, 0, 5) * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            transform.Translate(new Vector3(2, 0, 0) * Time.deltaTime);
+            p_Velocity += new Vector3(0, 0, 1);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(new Vector3(0, 0, -2) * Time.deltaTime);
+            p_Velocity += new Vector3(0, 0, -1);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Translate(new Vector3(-2, 0, 0) * Time.deltaTime);
+            p_Velocity += new Vector3(-1, 0, 0);
         }
-
-        if (Input.GetKey(KeyCode.E))
+        if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(0f, 0.5f, 0f, Space.Self);
+            p_Velocity += new Vector3(1, 0, 0);
         }
-        if (Input.GetKey(KeyCode.Q))
-        {
-            transform.Rotate(0f, -0.5f, 0f, Space.Self);
-        }
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            transform.Translate(new Vector3(0, 7, 0) * Time.deltaTime);
-        }
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            transform.Translate(new Vector3(0, -3.5f, 0) * Time.deltaTime);
-        }
-
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.name == "Floor")
-        {
-            Debug.Log("I have hit the floor!");
-        }
+        return p_Velocity;
     }
 }
-
